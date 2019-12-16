37c4031da8577890dd439af288a53013b67029c9
NASA <email4sun@163.com>
2019-12-16 16:47:07 +0800

    fix

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'email4sun@163.com'
+  config.mailer_sender = 'no-reply@email.51read.site'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
